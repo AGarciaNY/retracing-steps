b28964e231221bc52a5f8fe033d8cbb07e5412bb
AGarciaNY <aldairgarcia360@gmail.com>
2025-01-10 19:46:10 -0500

    first commit

---
+++ src/app/blog/page.js
@@ -0,0 +1,9 @@
+import Nav from "@/componets/Nav/nav";
+export default function blog() {
+    return (
+      <>
+        <Nav/>
+        <h1>blog here</h1>
+      </>
+    );
+  }
---
+++ src/componets/Nav/nav.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+export default function Nav() {
+    const links = [
+      {
+        name:"about",
+        href:"/about"
+      },
+      {
+        name:"blog",
+        href:"/blog"
+      },{
+        name:"home",
+        href:"/"
+      }
+    ]
+    return (
+      <>
+        {links.map((link) => {
+          const LinkIcon = link.icon;
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            >
+              <p className="hidden md:block">{link.name}</p>
+            </Link>
+          );
+        })}
+      </>  
+    );
+  }
+  
